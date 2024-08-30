050993b10830faab5d118be090eb3bae20d7810b
yongchane <202021113@sangmyung.kr>
2024-08-31 00:46:32 +0900

    #5 watchit param,쿼리 스트링 세팅

--- watchit/pages/movies/[id].js
+++ watchit/pages/movies/[id].js
@@ -1,3 +1,7 @@
+import { useRouter } from "next/navigation";
+
 export default function Movies() {
+  const router = useRouter();
+  const { id } = router.query;
   return <div>Movies 페이지</div>;
 }
--- watchit/pages/search.js
+++ watchit/pages/search.js
@@ -1,3 +1,12 @@
+import { useRouter } from "next/navigation";
+
 export default function Search() {
-  return <div>Search 페이지</div>;
+  const router = useRouter();
+  const { q } = router.query;
+  return (
+    <div>
+      <h1>watchit</h1>
+      <h2>{q} 검색 결과</h2>
+    </div>
+  );
 }
