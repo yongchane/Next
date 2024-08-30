b986641ce4c681598c7dc3ccd29ce8e0423a4be3
yongchane <202021113@sangmyung.kr>
2024-08-31 00:35:49 +0900

    #6 param 값과 쿼리스트링 가져오는 법

--- my-next-app/pages/products/[id].js
+++ my-next-app/pages/products/[id].js
@@ -1,3 +1,9 @@
+import { useRouter } from "next/router";
+
+//param 값 가져오기
 export default function Product() {
-  return <div>Product 페이지</div>;
+  const router = useRouter();
+  const { id } = router.query;
+  // 해당 id 는 파일 이름에 해당하는 id와 동일
+  return <div>Product {id}페이지</div>;
 }
--- my-next-app/pages/search.js
+++ my-next-app/pages/search.js
@@ -1,3 +1,13 @@
+import { useRouter } from "next/router";
+
+//쿼리 스트링 가져오기
 export default function Search() {
-  return <div>search 페이지</div>;
+  const router = useRouter();
+  const { q } = router.query;
+  return (
+    <div>
+      <h1>search 페이지</h1>
+      <h2>{q} 검색 결과</h2>
+    </div>
+  );
 }
