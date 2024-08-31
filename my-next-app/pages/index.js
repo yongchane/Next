808876471d1a527f3f7b7e2e9022c95787c6533f
yongchane <202021113@sangmyung.kr>
2024-08-31 16:03:19 +0900

    #6 axios 를 사용한 api 연동 실습

--- my-next-app/pages/index.js
+++ my-next-app/pages/index.js
@@ -1,26 +1,24 @@
+import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
-import styles from "@/styles/Home.module.css";
-import Link from "next/link";
+import { useEffect, useState } from "react";
+import axios from "@/lib/axios";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  async function getProducts() {
+    const res = await axios.get(`/products`);
+    const nextProducts = res.data.results;
+    setProducts(nextProducts);
+  }
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <div>
       <h1>Codeitmall</h1>
       <SearchForm />
-      <ul>
-        <li>
-          <Link href="/products/1">첫 번째 상품</Link>
-        </li>
-        <li>
-          <Link href="/products/2">두 번째 상품</Link>
-        </li>
-        <li>
-          <Link href="/products/3">세 번째 상품</Link>
-        </li>
-        <li>
-          <Link href="https://codeit.kr">코드잇</Link>
-        </li>
-      </ul>
+      <ProductList products={products} />
     </div>
   );
 }
--- my-next-app/pages/search.js
+++ my-next-app/pages/search.js
@@ -1,15 +1,29 @@
+import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
+import axios from "@/lib/axios";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 //쿼리 스트링 가져오기
 export default function Search() {
   const router = useRouter();
   const { q } = router.query;
+  const [products, setProducts] = useState([]);
+
+  async function getProducts(query) {
+    const res = await axios.get(`/products/?q=${query}`);
+    const nextProducts = res.data.results;
+    setProducts(nextProducts);
+  }
+  useEffect(() => {
+    getProducts(q);
+  }, [q]);
   return (
     <div>
       <h1>search 페이지</h1>
       <SearchForm initialValue={q} />
       <h2>{q} 검색 결과</h2>
+      <ProductList products={products} />
     </div>
   );
 }
