65c260c3fa89a2fcee77da8cc779cc73ee2f6d4a
yongchane <202021113@sangmyung.kr>
2024-08-31 19:34:32 +0900

    #6 리다이렉트 실습
    next.config.js에서 수정하여 리다이렉트 사용가능

--- my-next-app/components/ProductList.js
+++ my-next-app/components/ProductList.js
@@ -1,14 +1,18 @@
-import Link from 'next/link';
-import styles from './ProductList.module.css';
-import StarRating from './StarRating';
+import Link from "next/link";
+import styles from "./ProductList.module.css";
+import StarRating from "./StarRating";
 
-export default function ProductList({ className = '', products }) {
+export default function ProductList({ className = "", products }) {
   return (
     <ul className={`${styles.productList} ${className}`}>
       {products?.map((product) => (
         <li key={product.id}>
-          <Link className={styles.product} href={`/products/${product.id}`}>
-            <img className={styles.image} src={product.imgUrl} alt={product.name} />
+          <Link className={styles.product} href={`/items/${product.id}`}>
+            <img
+              className={styles.image}
+              src={product.imgUrl}
+              alt={product.name}
+            />
             <div className={styles.content}>
               <div>
                 <span className={styles.name}>{product.name}</span>
@@ -26,8 +30,7 @@ export default function ProductList({ className = '', products }) {
                   {product.starRatingCount.toLocaleString()}
                 </div>
                 <div className={styles.likeCount}>
-                  ♥
-                  {product.likeCount.toLocaleString()}
+                  ♥{product.likeCount.toLocaleString()}
                 </div>
               </div>
             </div>
--- my-next-app/next.config.mjs
+++ my-next-app/next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/products/:id",
+        // 리다이렉트 처리를 할 주소
+        //:id는 param의 이름이다
+        destination: "/items/:id",
+        // 이동시킬 주소
+        permanent: false,
+        //Reponse Satus Code를 정함
+        //웹 브라우저에게 주소가 바뀌었다는 사실을 저장하게 하려면 true 아니면 false
+      },
+    ];
+  },
 };
 
+//redirects라는 프로퍼티로 async 함수를 지정하는 문법
+//redirects 함수는 return 부분의 객체를 배열로 리턴하는 함수
 export default nextConfig;
