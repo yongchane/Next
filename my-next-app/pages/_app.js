1167b29cbc149c0a3fd6e18afcc1f99806d12d35
yongchane <202021113@sangmyung.kr>
2024-08-31 20:50:53 +0900

    #6 app 컴포넌트에 context 컴포넌트 불러서 전체 사용하기 실습

--- my-next-app/pages/_app.js
+++ my-next-app/pages/_app.js
@@ -1,14 +1,18 @@
 import Container from "@/components/Container";
 import Header from "@/components/Header";
+import { ThemeProvider } from "@/lib/ThemeContext";
 import "@/styles/global.css";
 
 export default function App({ Component, pageProps }) {
-  return;
-  <>
-    <Header />
-    <Container>
-      <Component {...pageProps} />;
-    </Container>
-  </>;
+  return (
+    <>
+      <ThemeProvider>
+        <Header />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  );
 }
 //Component 해당 prop은 쉽게 말해서 Next.js 페이지
--- my-next-app/pages/setting.js
+++ my-next-app/pages/setting.js
@@ -1,17 +1,26 @@
-import Container from '@/components/Container';
-import Header from '@/components/Header';
-import styles from '@/styles/Setting.module.css';
+import Dropdown from "@/components/Dropdown";
+import { useTheme } from "@/lib/ThemeContext";
+import styles from "@/styles/Setting.module.css";
 
 export default function Setting() {
+  const { theme, setTheme } = useTheme();
+
   return (
-    <>
-      <Header />
-      <Container>
-        <h1 className={styles.title}>설정</h1>
-        <section className={styles.section}>
-          <h2 className={styles.sectionTitle}>테마 설정</h2>
-        </section>
-      </Container>
-    </>
+    <div>
+      <h1 className={styles.title}>설정</h1>
+      <section className={styles.section}>
+        <h2 className={styles.sectionTitle}>테마 설정</h2>
+        <Dropdown
+          className={styles.input}
+          name="theme"
+          value={theme}
+          onChange={(name, value) => setTheme(value)}
+          options={[
+            { label: "라이트", value: "light" },
+            { label: "다크", value: "dark" },
+          ]}
+        />
+      </section>
+    </div>
   );
 }
