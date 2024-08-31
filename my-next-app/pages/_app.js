432cdddb9698ed15dc77a30c622753a5e04d836d
yongchane <202021113@sangmyung.kr>
2024-08-31 20:27:30 +0900

    #6 app컴포넌트를 사용한 전체 컴포넌트에 적용하기

--- my-next-app/pages/_app.js
+++ my-next-app/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/global.css';
+import Container from "@/components/Container";
+import Header from "@/components/Header";
+import "@/styles/global.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return;
+  <>
+    <Header />
+    <Container>
+      <Component {...pageProps} />;
+    </Container>
+  </>;
 }
+//Component 해당 prop은 쉽게 말해서 Next.js 페이지
