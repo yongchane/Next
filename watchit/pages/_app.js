631531a7880a6473d9b598adf2c50f223319be11
yongchane <202021113@sangmyung.kr>
2024-09-01 18:40:13 +0900

    #5 watchit  컴포넌트에 context 컴포넌트 불러서 전체 사용하기 실습

--- watchit/pages/_app.js
+++ watchit/pages/_app.js
@@ -1,11 +1,15 @@
+import Container from "@/components/Container";
+import Header from "@/components/Header";
+import { ThemeProvider } from "@/lib/ThemeContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return;
-  <>
-    <Header />
-    <Container page>
-      <Component {...pageProps} />;
-    </Container>
-  </>;
+  return (
+    <ThemeProvider>
+      <Header />
+      <Container page>
+        <Component {...pageProps} />
+      </Container>
+    </ThemeProvider>
+  );
 }
--- watchit/pages/setting.js
+++ watchit/pages/setting.js
@@ -1,17 +1,31 @@
-import Container from '@/components/Container';
-import Header from '@/components/Header';
-import styles from '@/styles/Setting.module.css';
+import Dropdown from "@/components/Dropdown";
+import { useTheme } from "@/lib/ThemeContext";
+import styles from "@/styles/Setting.module.css";
 
 export default function Setting() {
+  const { theme, setTheme } = useTheme();
+
+  function handleDropdownChange(name, value) {
+    const nextTheme = value;
+    setTheme(nextTheme);
+  }
+
   return (
     <>
-      <Header />
-      <Container page>
-        <h1 className={styles.title}>설정</h1>
-        <section className={styles.section}>
-          <h2 className={styles.sectionTitle}>테마 설정</h2>
-        </section>
-      </Container>
+      <h1 className={styles.title}>설정</h1>
+      <section className={styles.section}>
+        <h2 className={styles.sectionTitle}>테마 설정</h2>
+        <Dropdown
+          className={styles.dropdown}
+          name="theme"
+          value={theme}
+          options={[
+            { label: "다크", value: "dark" },
+            { label: "라이트", value: "light" },
+          ]}
+          onChange={handleDropdownChange}
+        />
+      </section>
     </>
   );
 }
